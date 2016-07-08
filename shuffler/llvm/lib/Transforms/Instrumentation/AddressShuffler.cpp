4a37b506b1510cff2124013ec4b1aa3e3fea1ad2
fang102 <fang102@purdue.edu>
2016-07-08 12:01:14 -0400

    Changed for AddressShuffler

--- shuffler/llvm/lib/Transforms/Instrumentation/AddressShuffler.cpp
+++ shuffler/llvm/lib/Transforms/Instrumentation/AddressShuffler.cpp
@@ -1,5 +1,4 @@
 #include "llvm/Transforms/Instrumentation.h"
-#include "llvm/ADT/DepthFirstIterator.h"
 #include "llvm/ADT/SmallString.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/SmallSet.h"
@@ -29,6 +28,48 @@
 #include "llvm/Target/TargetLibraryInfo.h"
 #include "llvm/Support/Process.h"
 #include "llvm/Support/FileSystem.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/DepthFirstIterator.h"
+#include "llvm/ADT/SetVector.h"
+#include "llvm/ADT/SmallSet.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/ADT/Triple.h"
+#include "llvm/Analysis/MemoryBuiltins.h"
+#include "llvm/IR/CallSite.h"
+#include "llvm/IR/DIBuilder.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/Dominators.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/InlineAsm.h"
+#include "llvm/IR/InstVisitor.h"
+#include "llvm/IR/IntrinsicInst.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/MDBuilder.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/MC/MCSectionMachO.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/DataTypes.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/Endian.h"
+#include "llvm/Support/SwapByteOrder.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/Instrumentation.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/Utils/ASanStackFrameLayout.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Transforms/Utils/Local.h"
+#include "llvm/Transforms/Utils/ModuleUtils.h"
+#include "llvm/Transforms/Utils/PromoteMemToReg.h"
+#include <algorithm>
+#include <string>
+#include <system_error>
 
 using namespace llvm;
 
@@ -40,6 +81,8 @@ namespace {
 	  bool runOnFunction(Function &F) override;
 	  bool doInitialization(Module &M) override;
 	  static char ID;
+	 private:
+	 	uint64_t getAllocaSizeInBytes(AllocaInst *AI);
 	};
 }  // namespace
 
@@ -58,7 +101,105 @@ bool AddressShuffler::doInitialization(Module &M) {
   return true;
 }
 
-bool AddressShuffler::runOnFunction(Function &F) {
-  llvm::errs() << "Compiling with AddressShuffler\n";
-  return false;
+uint64_t AddressShuffler::getAllocaSizeInBytes(AllocaInst *AI) {
+    uint64_t ArraySize = 1;
+    if (AI->isArrayAllocation()) {
+      ConstantInt *CI = dyn_cast<ConstantInt>(AI->getArraySize());
+      assert(CI && "non-constant array size");
+      ArraySize = CI->getZExtValue();
+    }
+    Type *Ty = AI->getAllocatedType();
+    uint64_t SizeInBytes =
+        AI->getModule()->getDataLayout().getTypeAllocSize(Ty);
+    return SizeInBytes * ArraySize;
 }
+
+
+bool AddressShuffler::runOnFunction(Function &F) {
+	llvm::errs() << "Compiling with AddressShuffler\n";
+	SmallSet<Value *, 16> TempsToInstrument;
+	SmallVector<Instruction *, 16> ToInstrument;
+	for (auto &BB : F) {
+		for (auto &Inst : BB) {
+			ToInstrument.push_back(&Inst);
+		}
+	}
+
+	int NumInstrumented = 0;
+	for (auto Inst : ToInstrument) {
+			if(isa<AllocaInst>(Inst)) {
+				// handle Alloca instruction
+				AllocaInst * AI = dyn_cast<AllocaInst>(Inst);
+				// get type of alloca inst
+				Type *Ty = AI->getAllocatedType();
+				// get size of alloca inst
+				int size = getAllocaSizeInBytes(AI);
+				Constant* AllocSize = ConstantExpr::getSizeOf(Ty);
+				// replacing alloca with malloc
+				Type * ITy = Type::getInt32Ty(getGlobalContext());
+				Instruction * Malloc = llvm::CallInst::CreateMalloc(Inst,
+                                             ITy, Ty, AllocSize,
+                                             nullptr, nullptr, "");
+				Inst->eraseFromParent();
+				//ReplaceInstWithInst(Inst, Malloc);
+				//llvm::errs() << "Before instrumentation :: Alloca: " << *Inst << "\n >> size = " << getAllocaSizeInBytes(AI) << "\n";
+				llvm::errs() << "After instrumentation :: " << * Malloc << "\n";
+
+			}
+			NumInstrumented++;
+	}
+	
+
+	// We want to instrument every address only once per basic block (unless there
+	// are calls between uses).
+	/*
+	SmallSet<Value *, 16> TempsToInstrument;
+	SmallVector<Instruction *, 16> ToInstrument;
+	SmallVector<Instruction *, 8> NoReturnCalls;
+	SmallVector<BasicBlock *, 16> AllBlocks;
+	SmallVector<Instruction *, 16> PointerComparisonsOrSubtracts;
+	int NumAllocas = 0;
+	bool IsWrite;
+	unsigned Alignment;
+	uint64_t TypeSize;
+
+	for (auto &BB : F) {
+	    AllBlocks.push_back(&BB);
+	    TempsToInstrument.clear();
+	    int NumInsnsPerBB = 0;
+	    for (auto &Inst : BB) {
+	      if (LooksLikeCodeInBug11395(&Inst)) return false;
+	      if (Value *Addr = isInterestingMemoryAccess(&Inst, &IsWrite, &TypeSize,
+	                                                  &Alignment)) {
+	        if (ClOpt && ClOptSameTemp) {
+	          if (!TempsToInstrument.insert(Addr).second)
+	            continue;  // We've seen this temp in the current BB.
+	        }
+	      } else if (ClInvalidPointerPairs &&
+	                 isInterestingPointerComparisonOrSubtraction(&Inst)) {
+	        PointerComparisonsOrSubtracts.push_back(&Inst);
+	        continue;
+	      } else if (isa<MemIntrinsic>(Inst)) {
+	        // ok, take it.
+	      } else {
+	        if (isa<AllocaInst>(Inst)) NumAllocas++;
+	        CallSite CS(&Inst);
+	        if (CS) {
+	          // A call inside BB.
+	          TempsToInstrument.clear();
+	          if (CS.doesNotReturn()) NoReturnCalls.push_back(CS.getInstruction());
+	        }
+	        if (CallInst *CI = dyn_cast<CallInst>(&Inst))
+	          maybeMarkSanitizerLibraryCallNoBuiltin(CI, TLI);
+	        continue;
+	      }
+	      ToInstrument.push_back(&Inst);
+	      NumInsnsPerBB++;
+	      if (NumInsnsPerBB >= ClMaxInsnsToInstrumentPerBB) break;
+	    }
+	}
+	*/
+
+	return false;
+}
+
