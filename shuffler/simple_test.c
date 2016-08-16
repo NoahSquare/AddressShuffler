a204d20b8f0b7ef45a447cb7fecc033bf76a5079
fang102 <fang102@purdue.edu>
2016-08-15 21:12:47 -0400

    Import allocator from Sanitizer to Shuffler but has not modified, buggy version

--- shuffler/simple_test.c
+++ shuffler/simple_test.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
-int g, m;
-char ch;
-
-void foo() {
-  printf("foo():\n g = %d\n ch = %c\n", g, ch);
-  g++;
-}
 
 int main() {
-  ch = 'a';
-  g = 1;
-  printf("main():\n g = %d\n", g);
-  foo();
-  printf("main():\n g++ = %d\n", g);
+  int a = 5;
 }
  
