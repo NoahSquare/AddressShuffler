db7b1d217dd014987695403dc23dde6ece83904d
Byoungyoung Lee <lifeasageek@gmail.com>
2016-09-01 15:53:27 -0400

    wip

--- todo/oram.py
+++ todo/oram.py
@@ -28,12 +28,53 @@ def write_block(block_id, block_data):
         # 3. write back
 
 
+class VMEM:
+    def oram_write(address, value):
+        pass
+
+    def oram_read(address)
+        pass
+
+def do_execute(inst, op1, op2, vregs, vmem):
+
+    # Everytime, need to invoke the same number of oram access.
+
+    mnemonic = get_mnemonic(inst)
+
+    if mnemonic == "MOV":
+        if is_mem_op(op1):
+            # op1 - memory poitner
+            # op2 - register
+            # *op1 = op2
+            vmem.oram_write(op1, vregs[op2])
+        elif is_mem_op(op2):
+            # op1 - register
+            # op2 - memory pointer
+            vregs[op1] = vmem.oram_read(op2)
+        else:
+            # op1 - register
+            # op2 - register
+            vregs[op1] = vregs[op2]
+
+    elif mnemonic == "ADD":
+        do_lea(op1, op2)
+
+    # ...
+
+def fetch_instruction(vmem, veip):
+    return vmem.oram_read(veip)
+
 def execution_loop():
     # virtual registers. Always stored in a real register file.
-    virtual = {}
-    while 1:
-        inst = fetch_instruction(virtual[eip])
-        op1 = fetch_op(I, 1)
-        op2 = fetch_op(I, 2)
+    vregs = {}
+    vregs[eip] = initial_virtual_eip
 
-        do_execute(inst, op1, op2)
+    # virtual memory
+    vmem = []
+
+    while 1:
+        inst = fetch_instruction(vmem, vregs[eip])
+        op1 = get_operand(I, 1)
+        op2 = get_operand(I, 2)
+
+        do_execute(inst, op1, op2, vregs, vmem)
